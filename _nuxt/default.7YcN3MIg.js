781aa94d067178c4307f264adfb38c03ccc49a5f
ahmedrangel <ahmedrangel@outlook.com>
2024-02-25 04:50:33 +0000

    Deploying to gh-pages from @ ahmedrangel/lnk.bayzamusic.com@c55bb01c33e4983d3ef452274fdf10f9fef21423 🚀

---
+++ _nuxt/default.7YcN3MIg.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper.DlAUqK2U.js";import{m as o,J as r,y as s}from"./entry.B6wF2CbC.js";const a={},c={id:"layout"};function n(e,_){return s(),o("div",c,[r(e.$slots,"default")])}const l=t(a,[["render",n]]);export{l as default};
